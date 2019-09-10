d3a1c64a8c7c423ce02d0d53c356bd6cb0d8dfa0
webdimension <office@webdimension.jp>
2019-09-10 18:43:31 +0900

    Mod Vagrant.Ver phpunit

--- Laravel/app/Http/Controllers/HomeController.php
+++ Laravel/app/Http/Controllers/HomeController.php
@@ -11,7 +11,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redis;
 //use Emitter;
 class HomeController extends Controller
-{
+
+ /**
+	* Show the application dashboard.
+
+ {
  /**
 	* Create a new controller instance.
 	*
@@ -20,11 +24,7 @@ class HomeController extends Controller
  public function __construct()
  {
 	$this->middleware('auth');
- }
-
- /**
-	* Show the application dashboard.
-	*
+ }*
 	* @return \Illuminate\Contracts\Support\Renderable
 	*/
  public function index()
