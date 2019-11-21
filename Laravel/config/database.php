c63c7d655d51a42d67f97cbf084cf09910741b95
webdimension <office@webdimension.jp>
2019-11-21 12:40:03 +0900

    Mod circleci.yml

--- Laravel/config/database.php
+++ Laravel/config/database.php
@@ -90,7 +90,7 @@ return [
                 'driver' => 'mysql',
                 'host' => '127.0.0.1',
                 'port' => '3306',
-                'database' => 'circleci_tournament',
+                'database' => 'circleci',
                 'username' => 'root',
                 'password' => '',
                 'charset' => 'utf8mb4',
