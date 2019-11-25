f3182bd4bab45b7cf97db3e1c3775ff31661eccb
webdimension <office@webdimension.jp>
2019-11-25 23:15:11 +0900

    Add certs

--- Laravel/tests/DuskTestCase.php
+++ Laravel/tests/DuskTestCase.php
@@ -21,9 +21,16 @@ abstract class DuskTestCase extends BaseTestCase
     {
 //        static::startChromeDriver();
     }
-    protected function baseUrl(){
-        return 'http://nginx:80';
+
+    protected function baseUrl()
+    {
+        if (env('APP_ENV') == 'local') {
+            return 'http://nginx:80';
+        } elseif (env('APP_ENV') == 'testing') {
+            return 'http://localhost:8000';
+        }
     }
+
     /**
      * Create the RemoteWebDriver instance.
      *
@@ -31,25 +38,28 @@ abstract class DuskTestCase extends BaseTestCase
      */
     protected function driver()
     {
-        $options = (new ChromeOptions)->addArguments([
-            '--disable-gpu',
-            '--headless',
-        ]);
-        return RemoteWebDriver::create(
-            'http://selenium-hub:4444/wd/hub', DesiredCapabilities::chrome()->setCapability(
-            ChromeOptions::CAPABILITY, $options
-        )
-        );
-//        $options = (new ChromeOptions)->addArguments([
-//            '--disable-gpu',
-//            '--headless',
-//            '--window-size=1920,1080',
-//        ]);
-//
-//        return RemoteWebDriver::create(
-//            'http://localhost:9515', DesiredCapabilities::chrome()->setCapability(
-//                ChromeOptions::CAPABILITY, $options
-//            )
-//        );
+        if (env('APP_ENV') == 'local') {
+            $options = (new ChromeOptions)->addArguments([
+                '--disable-gpu',
+                '--headless',
+            ]);
+            return RemoteWebDriver::create(
+                'http://selenium-hub:4444/wd/hub', DesiredCapabilities::chrome()->setCapability(
+                ChromeOptions::CAPABILITY, $options
+            )
+            );
+        } elseif (env('APP_ENV') == 'testing') {
+            $options = (new ChromeOptions)->addArguments([
+                '--disable-gpu',
+                '--headless',
+                '--window-size=1920,1080',
+           ]);
+            return RemoteWebDriver::create(
+//                'http://localhost:9515', DesiredCapabilities::chrome()->setCapability(
+                'http://localhost:4444/wd/hub', DesiredCapabilities::chrome()->setCapability(
+                ChromeOptions::CAPABILITY, $options
+            )
+            );
+        }
     }
 }
